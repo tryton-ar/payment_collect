b00ee0cd6d2a43af6ecc5559ab92dc8b4a3c7e98
Luciano Rossi <lukio@gcoop.coop>
2016-12-05 14:34:20 -0300

    Add field id to depends so it does not break in sao

--- paymode.py
+++ paymode.py
@@ -2,10 +2,9 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.model import fields, ModelSQL, ModelView
-from trytond.pool import PoolMeta, Pool
+from trytond.pool import Pool
 
 __all__ = ['PayMode']
-__metaclass__ = PoolMeta
 
 class PayMode(ModelSQL, ModelView):
     'Pay Mode'
