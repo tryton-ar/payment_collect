e585197985e7d0fa5a3b3b298d4e9feb2232f8a2
Luciano Rossi <lukio@gcoop.coop>
2019-03-22 06:42:08 -0300

    Remove property fields.

--- configuration.py
+++ configuration.py
@@ -1,18 +1,71 @@
 # This file is part of the payment_collect module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.model import fields
-from trytond.pool import PoolMeta
+from trytond.model import ModelView, ModelSQL, ModelSingleton, fields
+from trytond.model import MultiValueMixin, ValueMixin
+from trytond import backend
+from trytond.tools.multivalue import migrate_property
+from trytond.pyson import Eval
 from trytond.modules.account_invoice.invoice import STATES
+from trytond.modules.company.model import (
+    CompanyMultiValueMixin, CompanyValueMixin)
 
-__all__ = ['Configuration']
-__metaclass__ = PoolMeta
+__all__ = ['Configuration', 'ConfigurationPaymentCollectAccount',
+    'ConfigurationReceiptAccount']
 
 
-class Configuration:
-    __name__ = 'account.configuration'
-    default_payment_collect_journal = fields.Property(
-        fields.Many2One('account.journal', 'Default Payment Journal'))
-    when_collect_payment = fields.Property(
+class Configuration(
+        ModelSingleton, ModelSQL, ModelView, CompanyMultiValueMixin):
+    'PaymentCollect Configuration'
+    __name__ = 'payment_collect.configuration'
+    journal = fields.MultiValue(fields.Many2One(
+            'account.journal', "Journal", required=True,
+            domain=[
+                ('type', '=', 'revenue'),
+                ]))
+    when_collect_payment = fields.MultiValue(
         fields.Selection(STATES, 'When collect payment'))
-    collect_use_cron = fields.Boolean('Use Cron to pay invoices')
+    collect_use_cron = fields.MultiValue(
+        fields.Boolean('Use Cron to pay invoices')
+
+    @classmethod
+    def multivalue_model(cls, field):
+        pool = Pool()
+        if field == 'when_collect_payment':
+            return pool.get('payment_collect.configuration.account')
+        elif field == 'journal':
+            return pool.get('payment_collect.configuration..account')
+        elif field == 'collect_use_cron':
+            return pool.get('payment_collect.configuration..account')
+        return super(Configuration, cls).multivalue_model(field)
+
+    @classmethod
+    def default_when_collect_payment(cls, **pattern):
+        return cls.multivalue_model(
+            'when_collect_payment').default_when_collect_payment()
+
+    @classmethod
+    def default_collect_use_cron(cls, **pattern):
+        return cls.multivalue_model(
+            'collect_use_cron').default_collect_use_cron()
+
+
+class ConfigurationPaymentCollectAccount(ModelSQL, CompanyValueMixin):
+    "PaymentCollect Configuration Accounting"
+    __name__ = 'payment_collect.configuration.account'
+
+    journal = fields.Many2One(
+        'account.journal', "Journal",
+        domain=[
+            ('type', '=', 'revenue'),
+            ])
+    when_collect_payment = fields.Char('when_collect_payment')
+    collect_use_cron = fields.Boolean('collect_use_cron')
+
+    @classmethod
+    def default_when_collect_payment(cls):
+        return 'posted'
+
+    @classmethod
+    def default_collect_use_cron(cls):
+        return False
