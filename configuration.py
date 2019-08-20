44157775ea52eb5498b5abb09fb7e87bed22caab
Luciano Rossi <lukio@gcoop.coop>
2019-08-20 12:37:53 -0300

    REL #6 Borramos funcionalidad de PayWithCron en virtud de utilizar trytond-worker

--- configuration.py
+++ configuration.py
@@ -22,8 +22,6 @@ class Configuration(
             'account.invoice.payment.method', "Payment Method", required=True))
     when_collect_payment = fields.MultiValue(
         fields.Selection(STATES, 'When collect payment'))
-    collect_use_cron = fields.MultiValue(
-        fields.Boolean('Use Cron to pay invoices'))
     create_invoices = fields.MultiValue(
         fields.Boolean('Add button to create invoices at return'))
 
@@ -34,8 +32,6 @@ class Configuration(
             return pool.get('payment_collect.configuration.account')
         elif field == 'payment_method':
             return pool.get('payment_collect.configuration.account')
-        elif field == 'collect_use_cron':
-            return pool.get('payment_collect.configuration.account')
         elif field == 'create_invoices':
             return pool.get('payment_collect.configuration.account')
         return super(Configuration, cls).multivalue_model(field)
@@ -44,11 +40,6 @@ class Configuration(
     def default_when_collect_payment(cls, **pattern):
         return cls.multivalue_model(
             'when_collect_payment').default_when_collect_payment()
-
-    @classmethod
-    def default_collect_use_cron(cls, **pattern):
-        return cls.multivalue_model(
-            'collect_use_cron').default_collect_use_cron()
 
     @classmethod
     def default_create_invoices(cls, **pattern):
@@ -63,16 +54,11 @@ class ConfigurationPaymentCollectAccount(ModelSQL, CompanyValueMixin):
     payment_method = fields.Many2One('account.invoice.payment.method',
         "Payment Method")
     when_collect_payment = fields.Char('when_collect_payment')
-    collect_use_cron = fields.Boolean('collect_use_cron')
     create_invoices = fields.Boolean('Create invoice when process return')
 
     @classmethod
     def default_when_collect_payment(cls):
         return 'posted'
-
-    @classmethod
-    def default_collect_use_cron(cls):
-        return False
 
     @staticmethod
     def default_create_invoices():
