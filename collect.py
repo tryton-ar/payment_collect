ea5de8ea784ea5d1c12735892d8d34cbb758461d
Luciano Rossi <lukio@gcoop.coop>
2026-07-17 11:28:05 -0300

    fix get_origin() to use Model.get_name instead of search domain (2 instances in collect.py)

--- collect.py
+++ collect.py
@@ -309,11 +309,9 @@ class CollectSendStart(ModelView):
     @classmethod
     def get_origin(cls):
         Model = Pool().get('ir.model')
+        get_name = Model.get_name
         models = cls._get_origin()
-        models = Model.search([
-                ('model', 'in', models),
-                ])
-        return [(None, '')] + [(m.model, m.name) for m in models]
+        return [(None, '')] + [(m, get_name(m)) for m in models]
 
 
 class CollectSend(Wizard):
@@ -382,11 +380,9 @@ class CollectReturnStart(ModelView):
     @classmethod
     def get_origin(cls):
         Model = Pool().get('ir.model')
+        get_name = Model.get_name
         models = cls._get_origin()
-        models = Model.search([
-                ('model', 'in', models),
-                ])
-        return [(None, '')] + [(m.model, m.name) for m in models]
+        return [(None, '')] + [(m, get_name(m)) for m in models]
 
 
 class CollectReturn(Wizard):
